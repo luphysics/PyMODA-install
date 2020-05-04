680ec55ee5c5eae6b3c6cc4150fd9abba19a90f6
Sam McCormack <56069163+spmccormack@users.noreply.github.com>
2020-05-04 16:14:21 +0100

    Add macOS install script

---
+++ macos/install.sh
@@ -0,0 +1,19 @@
+echo "Downloading PyMODA. Please wait, this may take over a minute..."
+curl -fsSL "https://github.com/luphysics/PyMODA/releases/latest/download/PyMODA-macOS.dmg" -o PyMODA.dmg
+
+echo "Mounting .dmg file..."
+sudo hdiutil attach PyMODA.dmg
+
+echo "Copying application..."
+mkdir -p ~/.pymoda/PyMODA
+rm -r ~/.pymoda/PyMODA
+mkdir -p ~/.pymoda/PyMODA
+
+cp -r /Volumes/PyMODA/PyMODA.app ~/.pymoda/PyMODA/
+
+echo "Cleaning up..."
+sudo hdiutil detach /Volumes/PyMODA
+rm PyMODA.dmg
+
+echo "Launching PyMODA..."
+open ~/.pymoda/PyMODA/PyMODA.app
